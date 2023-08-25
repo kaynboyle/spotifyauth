6cdeebb168d1e64d5e3e9286bfb53fa532f24606
Kaylin Boyle <kaylinnboyle@gmail.com>
2023-08-24 17:08:19 -0700

    i got the api search to work and logout

--- spotifyauth-app/src/components/Main/index.jsx
+++ spotifyauth-app/src/components/Main/index.jsx
@@ -7,53 +7,73 @@ function Main()  {
     const  RESPONSE_TYPE = "token"    
         
         
-    const{token, setToken} = useState("")
-
-    useEffect( () => {
-      const hash = window.location.hash
-      let token = window.localStorage.getItem('token')
-
-      
-      if(!token && hash){
-        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split('=')[1]
-        
-        window.location.hash = ""
-        window.localStorage.setItem("token", token)
-        setToken(token)
-        console.log(token)
-        console.log("whee")
-        
-
+    
+    const [searchKey, setSearchKey] = useState("");
+    const [token, setToken] = useState('');
+  
+    useEffect(() => {
+      const hash = window.location.hash;
+      let storedToken = window.localStorage.getItem('token');
+  
+      if (!storedToken && hash) {
+        storedToken = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split('=')[1];
+        window.localStorage.setItem("token", storedToken);
       }
-    }, [])
+  
+      setToken(storedToken);
+    }, []);
+  
 
     const logout = () =>{
-      setToken("")
+      setToken("");
       window.localStorage.removeItem("token")
     }
-
-
-  return (
-    <div className="main">
-      <div>
-
-      {!token==token ?
-
-        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>login spotify</a>
-        : <button onClick={logout}>Logout</button>
+    const searchArtists = async (e) => {
+      e.preventDefault(); // Prevent default form submission
+    
+      if (searchKey.trim() === "") {
+        return; // Don't proceed with empty search key
       }
+    
+      try {
+        const response = await axios.get("https://api.spotify.com/v1/search", {
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+          params: {
+            q: searchKey,
+            type: "artist"
+          }
+        });
+    
+        console.log(response.data);
+        // Handle response data, update state, etc.
+      } catch (error) {
+        console.error("Error:", error);
+        // Handle the error, display a message, etc.
+      }
+    };
+    
 
-      {token==token ?
-        <form>
-          <input type="text"/>
-          <button type={"submit"}>Searches</button>
-        </form>
-        : <h2>Please Login</h2>
-        }
+    return (
+      <div className="main">
+        <div>
+          {!token ? (
+            <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>login spotify</a>
+          ) : (
+            <button onClick={logout}>Logout</button>
+          )}
+          
+          {token && (
+            <form onSubmit={searchArtists}>
+              <input type="text" value={searchKey} onChange={e => setSearchKey(e.target.value)} />
+              <button type="submit">Search</button>
+            </form>
+          )}
         </div>
-        
-    </div>
-  );
+      </div>
+    );
+    
 }
 
 export default Main;
